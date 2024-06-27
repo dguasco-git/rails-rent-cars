d6f81ab0d6850bed73274f5ff467eb69647830a8
Daniel Guasco <daniele.scultura@libero.it>
2024-06-27 15:20:03 +0200

    back-end complété

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,32 @@
 class BookingsController < ApplicationController
   def new
+    @car = Car.find(params[:car_id])
     @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.car = @car
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def index
+    @booking = Booking.all
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_at, :ends_at)
   end
 end
